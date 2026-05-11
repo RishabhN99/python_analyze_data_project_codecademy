6022de374292b1c1074b5f63c75f70dbfc5227eb
RishabhN99 <rishabhn9199@gmail.com>
2026-05-11 14:30:47 +0530

    Added more analysis to cereal project

--- script.py
+++ script.py
@@ -1,1 +1,5 @@
-import codecademylib3
+import numpy as np
+calorie_stats = np.genfromtxt('cereal.csv', delimiter=',')
+print(calorie_stats)
+average_calories = np.mean(calorie_stats) - 60
+print(average_calories)
